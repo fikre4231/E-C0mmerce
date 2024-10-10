698ae2708b45b96bcd84c7f398f07d046e4d9a28
fikre4231 <fikrebereda@gmail.com>
2024-10-10 04:05:41 -0700

    main

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -39,6 +39,7 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<HomePage />} />
+
           <Route path='/signup' element={!user ? <SignUpPage /> : <Navigate to='/' />} />
           <Route path="/login" element={!user ? <LoginPage /> : <Navigate to='/' />} />
           <Route
